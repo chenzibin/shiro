15378da3e46b22083b24dccc231a39bad6756a2e
zibin chen <chenzb@broada.com>
2017-08-02 21:39:22 +0800

    shiro-test\shiro-web

---
+++ shiro-test/src/test/java/LoginLogoutTest.java
@@ -0,0 +1,13 @@
+import org.apache.shiro.util.Factory;
+import org.junit.Test;
+
+/**
+ * Created by chenzb on 2017/8/2.
+ */
+public class LoginLogoutTest {
+
+    @Test
+    public void loginTest() {
+//        Factory<SecurityManager> factory =
+    }
+}
