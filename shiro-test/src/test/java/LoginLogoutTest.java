69c9d1b30b41a775e589c7b47d9eafbae6116f2f
chenzb <chenzb@broada.com>
2017-10-30 20:04:13 +0800

    第二章

--- shiro-test/src/test/java/LoginLogoutTest.java
+++ shiro-test/src/test/java/LoginLogoutTest.java
@@ -4,27 +4,38 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.Ini;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Iterator;
 
 /**
  * Created by chenzb on 2017/8/2.
  */
 public class LoginLogoutTest {
 
-    @Test
-    public void testLogin() {
-        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-realm.ini");
+//    @Test
+    public void testLogin(String source) {
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory(source);
         SecurityManager securityManager = factory.getInstance();
         SecurityUtils.setSecurityManager(securityManager);
         Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken("chenzb", "chenzb");
+        UsernamePasswordToken token = new UsernamePasswordToken("zhangsan", "123");
         try {
             subject.login(token);
         } catch (AuthenticationException e) {
             e.printStackTrace();
+        }
+
+        PrincipalCollection principalCollection = subject.getPrincipals();
+        Iterator it = principalCollection.asSet().iterator();
+        while (it.hasNext()) {
+            Object obj = it.next();
+            System.out.println();
         }
 
         Assert.assertEquals(true, subject.isAuthenticated());
@@ -35,5 +46,13 @@ public class LoginLogoutTest {
     public void testIni(){
         Ini ini = Ini.fromResourcePath("classpath:shiro-realm.ini");
         System.out.println();
+    }
+
+    @Test
+    public void testLogin2() {
+        String allSuccess = "classpath:shiro-authenticator-all-success.ini";
+        testLogin(allSuccess);
+
+
     }
 }
